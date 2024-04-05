d2d376a3316525ddeffc10e3f8165ec742fb97e2
WilyHyperion <ns9316@pleasantonusd.net>
2024-04-04 22:43:42 -0700

    fix error again

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -28,9 +28,46 @@ export default function Game() {
     const [score, setScore] = useState(0)
     const [time, setTime] = useState(1000)
     const [inputted, setInputted] = useState('')
-   
+
     useEffect(() => {
-        function checkNew(inputted: string){
+        window.onkeydown = (e) => {
+            if (inputted.length === currentGem.code.length) {
+                if (inputted === currentGem.code) {
+                    setScore(score + 1)
+                    setCurrentGem(selectedgems[Math.floor(Math.random() * selectedgems.length)])
+                    setInputted('')
+                } else {
+                    return
+                }
+            }
+            if (e.key == 'w' || e.key == 'ArrowUp') {
+                if (currentGem.code[inputted.length] == 'U') {
+                    setInputted(inputted + 'U',)
+                    return
+                }
+            } else if (e.key == 'a' || e.key == 'ArrowLeft') {
+                if (currentGem.code[inputted.length] == 'L') {
+                    setInputted(inputted + 'L')
+                    return
+                }
+            }
+            else if (e.key == 's' || e.key == 'ArrowDown') {
+                if (currentGem.code[inputted.length] == 'D') {
+                    setInputted(inputted + 'D')
+                    return
+                }
+            }
+            else if (e.key == 'd' || e.key == 'ArrowRight') {
+                if (currentGem.code[inputted.length] == 'R') {
+                    setInputted(inputted + 'R')
+                    return
+                }
+            }
+            setInputted('')
+
+
+        }
+        function checkNew(inputted: string) {
             if (inputted.length === currentGem.code.length) {
                 if (inputted === currentGem.code) {
                     setScore(score + 1)
@@ -43,57 +80,20 @@ export default function Game() {
         }
         checkNew(inputted)
     }, [currentGem.code, inputted, score, selectedgems])
-    window.onkeydown =  (e) => {
-        if (inputted.length === currentGem.code.length) {
-            if (inputted === currentGem.code) {
-                setScore(score + 1)
-                setCurrentGem(selectedgems[Math.floor(Math.random() * selectedgems.length)])
-                setInputted('')
-            } else {
-                return
-            }
-        }
-        if (e.key == 'w' || e.key == 'ArrowUp') {
-            if (currentGem.code[inputted.length] == 'U') {
-                setInputted(inputted + 'U', )
-                return
-            }
-        } else if (e.key == 'a' || e.key == 'ArrowLeft') {
-            if (currentGem.code[inputted.length] == 'L') {
-                setInputted(inputted + 'L')
-                return
-            }
-        }
-        else if (e.key == 's' || e.key == 'ArrowDown') {
-            if (currentGem.code[inputted.length] == 'D') {
-                setInputted(inputted + 'D')
-                return
-            }
-        }
-        else if (e.key == 'd' || e.key == 'ArrowRight') {
-            if (currentGem.code[inputted.length] == 'R') {
-                setInputted(inputted + 'R')
-                return
-            }
-        }
-        setInputted('')
-
-
-    }
     return (
         <Suspense>
-        <>
-            <Header />
-            <div id="game" className="flex flex-row items-center justify-center bg-gray-500 w-full min-h-[100vh]">
-                <div className="flex flex-col items-center gap-5 w-[80%] bg-black text-white">
-                    <Image src={'/gems/' + currentGem.name + '.svg'} alt={currentGem.name} width={100} height={100} />
-                    <h1 className='font-bold text-xl'>{currentGem.name}<br />
-                        {codeToArrow(currentGem.code) }<br/>
-                        {codeToArrow(inputted) + " "}
-                    </h1>
+            <>
+                <Header />
+                <div id="game" className="flex flex-row items-center justify-center bg-gray-500 w-full min-h-[100vh]">
+                    <div className="flex flex-col items-center gap-5 w-[80%] bg-black text-white">
+                        <Image src={'/gems/' + currentGem.name + '.svg'} alt={currentGem.name} width={100} height={100} />
+                        <h1 className='font-bold text-xl'>{currentGem.name}<br />
+                            {codeToArrow(currentGem.code)}<br />
+                            {codeToArrow(inputted) + " "}
+                        </h1>
+                    </div>
                 </div>
-            </div>
-        </>
+            </>
         </Suspense>
     )
 }
